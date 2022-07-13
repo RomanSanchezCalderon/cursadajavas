e7379374d2a5926239805ff6fb36b3d2f4f39e31
Roman Sanchez <isaacrsanchez@MacBook-Pro-de-Roman.local>
2022-07-12 21:55:28 -0500

    revision proyecto

--- script.js
+++ script.js
@@ -1,39 +1,69 @@
 
-let evento = prompt("Que tipo de evento tendrás").toLowerCase();
+function saludar () {
 
-switch (evento) {
-    case "boda":    
-        alert("Endulza el mejor momento de tu vida");
-        break;
+    let nombre = prompt("Hola, ¿cuál es tu nombre?");
+    alert( `Tu nombre es ${nombre}`);
 
-    case "graduacion":
-        alert("Disfruta no ir más a clases");
-        break;
-
-    case "fiesta familiar":
-        alert("Siempre somos el sabor de tu vida");
-        break;
-
-    default: alert("aun no contamos con la opcion para un(a) " + evento);
-    break;
 }
 
-let invitados = prompt("¿cuantos invitados tendrás?")
+saludar();
 
-    if (invitados <= 50){
-        alert("El paquete 1 es para ti");
-        
+function tipoevento () {
+
+    let evento = prompt("¿Qué tipo de evento tendrás?, Déjanos ser parte de él...").toLowerCase();
+
+    switch (evento) {
+        case "boda":    
+            alert("Endulza el mejor momento de tu vida");
+            break;
+
+        case "graduacion":
+            alert("Disfruta no ir más a clases");
+            break;
+
+        case "fiesta familiar":
+            alert("Siempre somos el sabor de tu vida");
+            break;
+
+        default: alert("Aun no contamos con la opcion para un(a) " + evento);
+        break;
+    }
+}
+
+tipoevento();
+
+
+var invitados = parseInt(prompt("¿Cuántos invitados tendrás?"));
+console.log(invitados)
+
+    if (invitados <= 250) {
+        alert("El paquete 1 es para ti")
+    } else if (invitados <= 500){
+        alert("El paquete 2 es para ti")    
+    } else if (invitados < 1000){
+        alert("El paquete 3 es para ti")
+    } else {
+        alert("No contamos con ese servicio :(")
     }
 
-    if (invitados <= 999){
-        alert("El paquete 2 es para ti");
-        
-    }
 
-     if (invitados > 999){
-        alert("El paquete 3 te está esperando");
-        
-    }
+function precioFinal (invitados) {
+
+    const precio = 30;
+    let valorFinal = precio * invitados;
+    alert ( `El costo es de ${valorFinal} pesos`);
+    return;  
+    
+}
+
+precioFinal (invitados);
+
+
+
+
+
+
+
 
 
 
